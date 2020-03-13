69e4f191acd626e4f538c39e33ce043b3c1fad97
haotian233 <33013299+haotian233@users.noreply.github.com>
2020-03-13 12:04:48 +0800

    Create Solution_BoyerMoore.cpp

---
+++ 多数元素/Solution_BoyerMoore.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = -1;
+        int count = 0;
+        for (int num : nums) {
+            if (num == candidate)
+                ++count;
+            else if (--count < 0) {
+                candidate = num;
+                count = 1;
+            }
+        }
+        return candidate;
+    }
+};
+/*Boyer-Moore 算法的本质和方法四中的分治十分类似。我们首先给出 Boyer-Moore 算法的详细步骤：
+
+    我们维护一个候选众数 candidate 和它出现的次数 count。初始时 candidate 可以为任意值，count 为 0；
+
+    我们遍历数组 nums 中的所有元素，对于每个元素 x，在判断 x 之前，如果 count 的值为 0，我们先将 x 的值赋予 candidate，随后我们判断 x：
+
+        如果 x 与 candidate 相等，那么计数器 count 的值增加 1；
+
+        如果 x 与 candidate 不等，那么计数器 count 的值减少 1。
+
+    在遍历完成后，candidate 即为整个数组的众数。
+*/
