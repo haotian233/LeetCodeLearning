400a3017e916957f1e5f262aa254703943c90453
haotian233 <33013299+haotian233@users.noreply.github.com>
2020-05-04 11:25:20 +0800

    Create 跳跃游戏.java

---
+++ 跳跃游戏(数组，贪心算法)/跳跃游戏.java
@@ -0,0 +1,16 @@
+//维护一个可以到达的最大距离的数组，如果下一位的坐标大于他这返回false
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int rightmost = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightmost) {
+                rightmost = Math.max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
